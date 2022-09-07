98bf25f1fec623f4e53732de9fcb28259ea0fd43
Anderson Dantas <andersondantas81@gmail.com>
2022-09-07 18:39:30 -0300

    Projeto web do mini jogo NFT World Cup

---
+++ src/Components/SelectCharacter/index.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import worldCup from "../../utils/WorldCup.json";
+import LoadingIndicator from "../LoadingIndicator";
+
+/*
+ * Não se preocupe com setCharacterNFT ainda, vamos falar dele logo.
+ */
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+
+  // UseEffect
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        worldCup.abi,
+        signer
+      );
+
+      /*
+       * Essa é a grande diferença. Configura nosso gameContract no estado.
+       */
+      setGameContract(gameContract);
+    } else {
+      console.log("Objeto Ethereum não encontrado");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Buscando contrato da seleção para mintar");
+
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTxn:", charactersTxn);
+
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+
+        setCharacters(characters);
+      } catch (error) {
+        console.error("Algo deu errado ao buscar seleção:", error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+      );
+
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log("CharacterNFT: ", characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+
+      /*
+       * Configurar NFT Minted Listener
+       */
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+
+    return () => {
+      if (gameContract) {
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  // Actions
+  const mintCharacterNFTAction = (characterId) => async () => {
+    try {
+      if (gameContract) {
+        setMintingCharacter(true);
+        console.log("Mintando seleção...");
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log("mintTxn:", mintTxn);
+        setMintingCharacter(false);
+      }
+    } catch (error) {
+      console.warn("MintCharacterAction Error:", error);
+      setMintingCharacter(false);
+    }
+  };
+
+  // Métodos de renderização
+  const renderCharacters = () =>
+    characters.map((character, index) => (
+      <div className="character-item" key={character.name}>
+        <div className="name-container">
+          <p>{character.name}</p>
+        </div>
+        <img
+          src={`https://cloudflare-ipfs.com/ipfs/${character.imageURI}`} alt={character.name}
+        />
+
+        {/* <img src={character.imageURI} alt={character.name} /> */}
+        <button
+          type="button"
+          className="character-mint-button"
+          onClick={mintCharacterNFTAction(index)}
+        >{`Mintar ${character.name}`}</button>
+      </div>
+    ));
+
+  return (
+    <div className="select-character-container">
+      <h2>Minte sua seleção. Escolha com sabedoria.</h2>
+      {/* Só mostra isso se tiver personagens no estado
+       */}
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
+      {mintingCharacter && (
+        <div className="loading">
+          <div className="indicator">
+            <LoadingIndicator />
+            <p>Mintando personagem...</p>
+          </div>
+          <img
+            src="http://pa1.narvii.com/6623/1d810c548fc9695d096d54372b625d207373130a_00.gif"
+            alt="Indicador de Mintagem"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = "0xd3ECe58319E165467A640842E87AD5075576ed62";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
+
